f81e80cc9d8b341f8a15b91620479be305c4f813
Răzvan Morcov-Pahoncea <razvan10190@gmail.com>
2022-05-12 14:05:24 +0300

    added routing for top10

--- First iteration/1st week progress/ip-movie-management/src/app/top10/top10.component.ts
+++ First iteration/1st week progress/ip-movie-management/src/app/top10/top10.component.ts
@@ -1,6 +1,7 @@
 import {MovieModel} from "../../models/movie-model";
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { IgxCarouselComponent } from 'igniteui-angular';
+import axios from "axios";
 
 @Component({
   selector: 'app-carousel',
@@ -11,7 +12,9 @@ import { IgxCarouselComponent } from 'igniteui-angular';
 export class Top10Component implements OnInit {
   @ViewChild('carousel', { static: true }) public carousel: IgxCarouselComponent | undefined;
 
-  public slides: any[] = [];
+  getTop10Endpoint = "http://127.0.0.1:8000/api/get_top10/"
+
+  public slides: MovieModel[] = [];
   public animations = ['slide', 'fade', 'none'];
 
   public ngOnInit() {
@@ -19,59 +22,65 @@ export class Top10Component implements OnInit {
   }
 
   public addSlides() {
-    this.slides.push(
-      {
-
-        heading: 'Titanic',
-        image: 'https://cache.moviestillsdb.com/i/500x/uwofcs46/titanic-lg.jpg',
-        link:  'https://www.imdb.com/title/tt0120338/'
-      },
-      {
-        heading: 'The Godfather',
-        image:'https://johnrieber.files.wordpress.com/2020/09/the-godfather-part-3-re-edited-version-2020.jpeg',
-        link: 'https://www.imdb.com/title/tt0068646/'
-      },
-      {
-        heading: 'The Dark Knight',
-        image: 'https://static.wikia.nocookie.net/1154b4c6-d018-4bc7-9d3a-33fd1d76acaf/scale-to-width-down/800',
-        link: 'https://www.imdb.com/title/tt0468569/'
-      },
-      {
-        heading: '12 Angry Men',
-        image: 'https://pbs.twimg.com/media/FKrN4CxXIAM_WfW.jpg',
-        link: 'https://www.imdb.com/title/tt0050083/'
-      },
-      {
-        heading: 'Child\'s Play',
-        image: 'https://i1.wp.com/welovemoviesmorethanyou.com/wp-content/uploads/2019/06/Childs_Play_UK_Poster.jpg?resize=800%2C500',
-        link: 'https://www.imdb.com/title/tt0167260/?ref_=nv_sr_srsg_0'
-      },
-      {
-        heading: 'Pulp Fiction',
-        image: 'https://4.bp.blogspot.com/-DP_qitYM-7M/XgMdfLRqBwI/AAAAAAAAIhA/zwCMYlA5aJoLnCgbG8WQYfKxNOy82tZ3ACK4BGAYYCw/w1200-h630-p-k-no-nu/pulp%2Bfiction.png',
-        link: 'https://www.imdb.com/title/tt0110912/'
-      },
-      {
-        heading: 'Forrest Gump',
-        image: 'https://1.bp.blogspot.com/-tTE8xYvzW30/YF3QYOLue_I/AAAAAAAACV8/zIhc_mVsFs8b2-hURw-UUg0OsBIjhqwCgCLcBGAsYHQ/w1200-h630-p-k-no-nu/wes-anderson-style-forrest-gump-opening-title-sequence.jpeg\n',
-        link: ' https://www.imdb.com/title/tt0109830/?ref_=fn_al_tt_1'
-      },
-      {
-        heading: 'The Silence of the Lambs',
-        image: 'https://cdn.eventfinda.co.nz/uploads/events/transformed/1414496-622576-14.jpg',
-        link: 'https://www.imdb.com/title/tt0102926/?ref_=fn_al_tt_1'
-      },
-      {
-        heading: 'Star Wars: The Last Jedi',
-        image: 'https://www.nation.sc/images/2018/Jan/13/star-wars-the-last-jedi-poster-700x1037-1.jpg',
-        link: 'https://www.imdb.com/title/tt0076759/?ref_=fn_al_tt_1'
-      },
-      {
-        heading: 'Joker',
-        image: 'https://w0.peakpx.com/wallpaper/219/957/HD-wallpaper-joker-2019-poster-promotional-materials-main-character-joaquin-phoenix.jpg',
-        link: 'https://www.imdb.com/title/tt7286456/?ref_=fn_al_tt_1'
+    axios.get(this.getTop10Endpoint).then(response => {
+        this.slides = response.data
       }
+    ).catch()
 
-    );
+    console.log(this.slides)
+    // this.slides.push(
+    //   {
+    //
+    //     heading: 'Titanic',
+    //     image: 'https://cache.moviestillsdb.com/i/500x/uwofcs46/titanic-lg.jpg',
+    //     link:  'https://www.imdb.com/title/tt0120338/'
+    //   },
+    //   {
+    //     heading: 'The Godfather',
+    //     image:'https://johnrieber.files.wordpress.com/2020/09/the-godfather-part-3-re-edited-version-2020.jpeg',
+    //     link: 'https://www.imdb.com/title/tt0068646/'
+    //   },
+    //   {
+    //     heading: 'The Dark Knight',
+    //     image: 'https://static.wikia.nocookie.net/1154b4c6-d018-4bc7-9d3a-33fd1d76acaf/scale-to-width-down/800',
+    //     link: 'https://www.imdb.com/title/tt0468569/'
+    //   },
+    //   {
+    //     heading: '12 Angry Men',
+    //     image: 'https://pbs.twimg.com/media/FKrN4CxXIAM_WfW.jpg',
+    //     link: 'https://www.imdb.com/title/tt0050083/'
+    //   },
+    //   {
+    //     heading: 'Child\'s Play',
+    //     image: 'https://i1.wp.com/welovemoviesmorethanyou.com/wp-content/uploads/2019/06/Childs_Play_UK_Poster.jpg?resize=800%2C500',
+    //     link: 'https://www.imdb.com/title/tt0167260/?ref_=nv_sr_srsg_0'
+    //   },
+    //   {
+    //     heading: 'Pulp Fiction',
+    //     image: 'https://4.bp.blogspot.com/-DP_qitYM-7M/XgMdfLRqBwI/AAAAAAAAIhA/zwCMYlA5aJoLnCgbG8WQYfKxNOy82tZ3ACK4BGAYYCw/w1200-h630-p-k-no-nu/pulp%2Bfiction.png',
+    //     link: 'https://www.imdb.com/title/tt0110912/'
+    //   },
+    //   {
+    //     heading: 'Forrest Gump',
+    //     image: 'https://1.bp.blogspot.com/-tTE8xYvzW30/YF3QYOLue_I/AAAAAAAACV8/zIhc_mVsFs8b2-hURw-UUg0OsBIjhqwCgCLcBGAsYHQ/w1200-h630-p-k-no-nu/wes-anderson-style-forrest-gump-opening-title-sequence.jpeg\n',
+    //     link: ' https://www.imdb.com/title/tt0109830/?ref_=fn_al_tt_1'
+    //   },
+    //   {
+    //     heading: 'The Silence of the Lambs',
+    //     image: 'https://cdn.eventfinda.co.nz/uploads/events/transformed/1414496-622576-14.jpg',
+    //     link: 'https://www.imdb.com/title/tt0102926/?ref_=fn_al_tt_1'
+    //   },
+    //   {
+    //     heading: 'Star Wars: The Last Jedi',
+    //     image: 'https://www.nation.sc/images/2018/Jan/13/star-wars-the-last-jedi-poster-700x1037-1.jpg',
+    //     link: 'https://www.imdb.com/title/tt0076759/?ref_=fn_al_tt_1'
+    //   },
+    //   {
+    //     heading: 'Joker',
+    //     image: 'https://w0.peakpx.com/wallpaper/219/957/HD-wallpaper-joker-2019-poster-promotional-materials-main-character-joaquin-phoenix.jpg',
+    //     link: 'https://www.imdb.com/title/tt7286456/?ref_=fn_al_tt_1'
+    //   }
+    //
+    // );
   }
 }
